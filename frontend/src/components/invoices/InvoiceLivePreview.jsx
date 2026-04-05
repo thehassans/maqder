ebaa0f01ca5699c6c1a69504453b7d61011712bf
thehassans <thehassans@github.com>
2026-04-06 01:11:11 +0300

    Lower Riyal icon more in invoice snapshots

--- frontend/src/components/invoices/InvoiceLivePreview.jsx
+++ frontend/src/components/invoices/InvoiceLivePreview.jsx
@@ -26,7 +26,7 @@ const renderSarMoneySnapshotIcon = ({ formatted, className = '' }) => (
         marginInlineEnd: '0.18em',
         verticalAlign: '-0.12em',
         position: 'relative',
-        top: '0.08em',
+        top: '0.11em',
       }}
     />
     <span className="inline leading-none">{formatted}</span>
