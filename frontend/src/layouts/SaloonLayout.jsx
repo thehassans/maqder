8e27575f5f21e065cfef1ae7a21ba88cb4d57ef2
thehassans <thehassans@github.com>
2026-05-28 05:44:22 +0300

    style: make saloon layout and sidebar match main ERP theme

--- frontend/src/layouts/SaloonLayout.jsx
+++ frontend/src/layouts/SaloonLayout.jsx
@@ -1,21 +1,22 @@
 import { Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 import Header from '../components/layout/Header'
 import SaloonSidebar from '../components/layout/SaloonSidebar'
 
 export default function SaloonLayout() {
-  return (
-    <div className="flex h-screen bg-gray-50 dark:bg-dark-900 overflow-hidden">
-      <div className="hidden lg:block w-64 bg-white dark:bg-dark-800 border-e border-gray-100 dark:border-dark-700 h-full overflow-y-auto">
-        <SaloonSidebar />
-      </div>
+  const { sidebarCollapsed } = useSelector((state) => state.ui)
 
-      <div className="flex-1 flex flex-col h-full min-w-0">
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-dark-900">
+      <SaloonSidebar />
+      <div
+        className={`transition-all duration-300 ${
+          sidebarCollapsed ? 'lg:ms-20' : 'lg:ms-72'
+        }`}
+      >
         <Header />
-        
-        <main className="flex-1 overflow-y-auto w-full relative">
-          <div className="max-w-[1600px] mx-auto p-4 md:p-6 lg:p-8 h-full">
-            <Outlet />
-          </div>
+        <main className="p-4 lg:p-6">
+          <Outlet />
         </main>
       </div>
     </div>
