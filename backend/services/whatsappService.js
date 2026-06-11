de1c6f906bc75b016aeb36526b3665964aafe43c
thehassans <thehassans@github.com>
2026-06-11 04:19:04 +0300

    fix(whatsapp): add webVersionCache to fix whatsapp-web.js initialization hang due to WA Web updates

--- backend/services/whatsappService.js
+++ backend/services/whatsappService.js
@@ -38,6 +38,10 @@ class WhatsAppService {
           '--no-zygote',
           '--disable-gpu'
         ]
+      },
+      webVersionCache: {
+        type: 'remote',
+        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
       }
     });
 
